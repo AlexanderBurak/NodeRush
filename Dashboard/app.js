f1e2102f913a2bc2bdcf5e4910aa1e36b0f404c8
Alex Burak <alexandr.burak@gmail.com>
2015-05-04 18:35:38 +0300

    Added config folder

--- Dashboard/app.js
+++ Dashboard/app.js
@@ -9,10 +9,22 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var swig  = require('swig');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'swig');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,6 +36,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+
+// required for passport
+app.use(session({ secret: 'alexburak' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Dashboard/config/database.js
@@ -0,0 +1,3 @@
+/**
+ * Created by alexander.burak on 04.05.2015.
+ */
